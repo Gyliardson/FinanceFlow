bbc494644b5ffa86d818d9a5013bbc8d17cb5653
Gyliardson Keitison <gylifodaot@gmail.com>
2026-03-27 23:46:11 -0300

    feat(mobile): enable sticky-immersive mode for Android navigation bar

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { Platform } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import AppNavigator from './src/navigation/AppNavigator';
+import * as NavigationBar from 'expo-navigation-bar';
+import { StatusBar } from 'expo-status-bar';
 
 export default function App() {
+  useEffect(() => {
+    if (Platform.OS === 'android') {
+      // Configura o modo imersivo "sticky": as barras somem e aparecem ao deslizar
+      NavigationBar.setBehaviorAsync('sticky-immersive');
+      NavigationBar.setVisibilityAsync('hidden');
+    }
+  }, []);
+
   return (
     <NavigationContainer>
+      <StatusBar style="auto" />
       <AppNavigator />
     </NavigationContainer>
   );
